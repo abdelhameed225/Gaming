10a80bf066fbe383425a1838f03b642b9ac55593
Abdelhameed <abdelhameedellaban981@gmail.com>
2023-11-02 05:29:35 +0200

    final5.29

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstCompComponent } from './Component/first-comp/first-comp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component:FirstCompComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

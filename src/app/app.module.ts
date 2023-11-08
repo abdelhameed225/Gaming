ed680c377ddfb2012bcbbbb91b736014735f4516
Ahmed Atef <Aahmedaatef96@gmail.com>
2023-11-08 18:57:42 +0200

    All Edites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import {
 } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NabbarComponent } from './Component/nabbar/nabbar.component';
